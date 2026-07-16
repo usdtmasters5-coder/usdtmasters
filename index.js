9c27f17378fb3123f5d9e94be512ff081a20ceea
usdtmasters5-coder <usdtmasters5@gmail.com>
2026-07-16 18:38:37 +0100

    Create index.js
    Telegram bot code

---
+++ index.js
@@ -0,0 +1,9 @@
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = process.env.BOT_TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", (msg) => {
+  bot.sendMessage(msg.chat.id, "Welcome to USDT Masters Education");
+});
